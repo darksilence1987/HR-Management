686be39232a02a98466c61fa7651e2d32ffbd4dc
Kaan Kara <kaankara1987@gmail.com>
2022-12-07 14:19:02 +0300

    Initial commit

---
+++ config-server/src/main/java/org/team3/ConfigServerApplication.java
@@ -0,0 +1,11 @@
+package org.team3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
