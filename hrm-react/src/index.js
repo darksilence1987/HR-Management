11eafe2d04bc34c1f238949e9e805963198eb9aa
BURAK600 <burakozer539@gmail.com>
2022-12-13 10:41:56 +0300

    react login register ok

--- hrm-react/src/index.js
+++ hrm-react/src/index.js
@@ -2,14 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { Provider } from "react-redux";
+import {Provider} from "react-redux";
 import store from "./store/store";
+import {DevSupport} from "@react-buddy/ide-toolbox";
+import {ComponentPreviews, useInitial} from "./dev";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
         <Provider store={store}>
-            <App />
+            <DevSupport ComponentPreviews={ComponentPreviews}
+                        useInitialHook={useInitial}
+            >
+                <App/>
+            </DevSupport>
         </Provider>
     </React.StrictMode>
 );
