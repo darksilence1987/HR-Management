137f7af1fa21ea784f2b9cb18994a08de7f7b3c6
Eda Senem BIYIK <96740740+edasenembiyik@users.noreply.github.com>
2022-12-28 16:10:27 +0300

    Hata düzeltme

--- hrm-react/src/config/UserService.js
+++ hrm-react/src/config/UserService.js
@@ -6,7 +6,10 @@ const userService = {
     findbyemail: RestApis.userService + "/user/get-user-by-email",
     updateuserfromuser: RestApis.userService + "/user/updateuserfromuser/{email}",
     usercreate: RestApis.userService + "/user/usercreate",
-    findAllManager: RestApis.userService + "/user/assignmanager/{email}",
+    findAllManager: RestApis.userService + "/user/getallmanagersssummaryinfo",
+    assignManager: RestApis.userService + "/user/assignmanager/{email}",
+
+
 
 };
 
